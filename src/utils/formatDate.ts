e6a45b1b0e667f4c6a0c4fb5788d3c93d8f6c2e4
tan-le <tan.le@monstar-lab.com>
2023-03-01 14:03:56 +0700

    refactor format date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,6 @@
 export const formatDate = (timestamp: number) => {
   const date = new Date(timestamp);
-  return `${date.getMonth() + 1} ${date.getDate()}, ${date.getFullYear()}`;
+  return `${date.toLocaleDateString("US", {
+    month: "long",
+  })} ${date.getDate()}, ${date.getFullYear()}`;
 };
